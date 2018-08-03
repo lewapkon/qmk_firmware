710c99f419288aa2c1f1abfc6ddaa9ef7b8a1934
Paweł Koniarski <me@pkoniarski.com>
2018-08-04 00:52:56 +0200

    Added some macros

--- keyboards/planck/keymaps/lewapkon/keymap.c
+++ keyboards/planck/keymaps/lewapkon/keymap.c
@@ -27,10 +27,17 @@ enum planck_layers {
   _ADJUST
 };
 
+enum planck_keycodes {
+  QWERTY = SAFE_RANGE,
+  GAME,
+  NAME,
+  USER,
+  MAIL,
+  SCRFULL,
+  SCRRECT
+};
 
 #define SFT_ENT RSFT_T(KC_ENT)
-#define QWERTY  DF(_QWERTY)
-#define GAME    DF(_GAME)
 #define LOWER   MO(_LOWER)
 #define RAISE   MO(_RAISE)
 
@@ -110,24 +117,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Adjust (Lower + Raise)
  * ,-----------------------------------------------------------------------------------.
- * | Del  | Reset|      |      |      |      |      |      |      |      |      | Del  |
+ * | Del  | Reset|      |      |      |      |      | Name | User | Mail |      | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |Aud on|Audoff|AGnorm|AGswap|Qwerty| Game |      |      |      |
+ * |      |      |MusMod|Aud on|Audoff|AGnorm|AGswap|Qwerty| Game |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|Pause |Break |PrtScr|      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-  KC_DEL,  RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, KC_DEL,
-  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  GAME,     _______, _______, _______,
-  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+  KC_DEL,  RESET,   _______, SCRFULL, SCRRECT, _______, _______, NAME,    USER,    MAIL,    _______, KC_DEL,
+  _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  GAME,    _______, _______, _______,
+  _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  KC_PAUS, KC_BRK,  KC_PSCR, _______, _______,
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
 
 uint32_t layer_state_set_user(uint32_t state) {
   return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case GAME:
+      if (record->event.pressed) {
+        set_single_persistent_default_layer(_GAME);
+      }
+      return false;
+      break;
+    case NAME:
+      if (record->event.pressed) {
+        send_string("Pawe" SS_RALT("l") " Koniarski");
+      }
+      return false;
+      break;
+    case USER:
+      if (record->event.pressed) {
+        send_string("lewapkon");
+      }
+      return false;
+      break;
+    case MAIL:
+      if (record->event.pressed) {
+        send_string("pawel@koniar.ski");
+      }
+      return false;
+      break;
+    case SCRFULL:
+      if (record->event.pressed) {
+        send_string(SS_LSFT(SS_LGUI("3")));
+      }
+      return false;
+      break;
+    case SCRRECT:
+      if (record->event.pressed) {
+        send_string(SS_LSFT(SS_LGUI("4")));
+      }
+      return false;
+      break;
+  }
+  return true;
 }
